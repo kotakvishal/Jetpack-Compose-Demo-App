44ae0c682dfcb7a1e81f787894db64298acd641e
kotakvishal <kotakvishal2001@gmail.com>
2024-04-20 12:49:03 +0530

    Impl Rows & Columns.

--- app/src/main/java/ktk/bugdroid/jetpackcomposedemoapp/MainActivity.kt
+++ app/src/main/java/ktk/bugdroid/jetpackcomposedemoapp/MainActivity.kt
@@ -3,44 +3,50 @@ package ktk.bugdroid.jetpackcomposedemoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import ktk.bugdroid.jetpackcomposedemoapp.ui.theme.JetpackComposeDemoAppTheme
+import androidx.compose.ui.unit.dp
+import org.w3c.dom.Text
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposeDemoAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+            Column {
+                // Divide the screen into two equal parts
+                Row(modifier = Modifier.weight(1f)) {
+                    Surface(modifier = Modifier.fillMaxSize(), color = Color.Black) {
+                        // Content for the first part
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceEvenly
+                        ) {
+                            Text(text = "Hii ", color = Color.White)
+                            Text(text = "this is row", color = Color.White)
+                        }
+                    }
+                }
+                Row(modifier = Modifier.weight(1f)) {
+                    Surface(modifier = Modifier.fillMaxSize(), color = Color.Red) {
+                        // Content for the second part
+                        Column(
+                            verticalArrangement = Arrangement.SpaceAround,
+                            horizontalAlignment = Alignment.End
+                        ) {
+                            Text(text = "Hiii ", color = Color.Yellow)
+                            Text(text = "this is column", color = Color.Yellow)
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeDemoAppTheme {
-        Greeting("Android")
     }
 }
