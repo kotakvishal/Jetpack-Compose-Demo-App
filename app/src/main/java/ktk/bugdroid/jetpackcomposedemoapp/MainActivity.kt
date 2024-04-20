4ce0439f8980a051453f172f1233f12b926a8ace
kotakvishal <kotakvishal2001@gmail.com>
2024-04-20 11:30:07 +0530

    Initial Commit

---
+++ app/src/main/java/ktk/bugdroid/jetpackcomposedemoapp/MainActivity.kt
@@ -0,0 +1,46 @@
+package ktk.bugdroid.jetpackcomposedemoapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ktk.bugdroid.jetpackcomposedemoapp.ui.theme.JetpackComposeDemoAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeDemoAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    JetpackComposeDemoAppTheme {
+        Greeting("Android")
+    }
+}
